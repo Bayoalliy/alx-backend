fe320749adb1f498a03af5941e4b2122f36186fd
bayoalliy <adebayoalliy@gmail.com>
2024-11-02 16:35:41 +0000

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,7 @@ This module contains a function that returns a tuple.
 
 
 def index_range(page, page_size):
+    """ index range finder """
     last_index = page * page_size
     first_index = last_index - page_size
     return((first_index, last_index))
