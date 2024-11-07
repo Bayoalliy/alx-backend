055b49fe39af767f86ad3f15aa2fd09eae0a2026
bayoalliy <adebayoalliy@gmail.com>
2024-11-07 10:51:49 +0000

    lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,22 +16,19 @@ class LRUCache(BaseCaching):
     def put(self, key, item):
         """store data in the cache"""
         if key and item:
-            if len(self.cache_data) + 1 > self.MAX_ITEMS:
-                least_w = list(self.acces.values())
-                least_w.sort()
-                least = least_w[0]
-                for k in self.acces.keys():
-                    if key in self.cache_data.keys():
-                        break
-                    if self.acces.get(k) == least:
-                        print(self.acces)
-                        print("DISCARD: {}".format(k))
-                        del(self.cache_data[k])
-                        del(self.acces[k])
-                        break
-
             self.cache_data[key] = item
             self.acces[key] = self.weight
+        if len(self.cache_data) > self.MAX_ITEMS:
+            least_w = list(self.acces.values())[:-1]
+            least_w.sort()
+            least = least_w[0]
+            for k in list(self.acces.keys())[:-1]:
+                if self.acces.get(k) == least:
+                    print(self.acces)
+                    print("DISCARD: {}".format(k))
+                    del(self.cache_data[k])
+                    del(self.acces[k])
+                    break
 
     def get(self, key):
         """retrieves data from thr cache"""
